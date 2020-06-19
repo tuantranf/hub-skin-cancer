0b7491dce7d43101765d1ab8dc0607a622cdfba1
aliabd <ali.si3luwa@gmail.com>
2020-06-19 07:41:23 -0700

    fixed gradio-config and added model.h5

--- gradio-config.py
+++ gradio-config.py
@@ -3,8 +3,7 @@ import base64
 from tensorflow.keras.models import load_model
 import numpy as np
 import gradio
-import src
-
+from src import moleimages
 
 def load():
     model = load_model("./models/mymodel-2.h5")
@@ -20,16 +19,16 @@ def predict(inp, model):
     inp = base64.b64decode(inp)
     img = io.imread(inp, plugin='imageio')
 
-    mimg = src.moleimages.MoleImages()
+    mimg = moleimages.MoleImages()
     X = mimg.load_image(img)
 
     y_pred = model.predict(X)
-    return {"benign": y_pred, "cancerous": 1-y_pred}
+    return {"benign": float(y_pred[0][0]), "cancerous": float(1-y_pred[0][0])}
 
 
-INPUTS = gradio.inputs.ImageIn()
+INPUTS = gradio.inputs.ImageIn(cast_to="base64")
 OUTPUTS = gradio.outputs.Label()
 INTERFACE = gradio.Interface(fn=predict, inputs=INPUTS, outputs=OUTPUTS,
-                             load_fn=load)
+                             load_fn=load, capture_session=True)
 
-INTERFACE.launch()
+INTERFACE.launch(inbrowser=True)
